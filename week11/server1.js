2dcc23f0c81fd3c77693e57dfba261abc62f3843
nenavathnaveen9904 <ugs24123_it.naveen@cbit.org.in>
2026-05-08 11:00:03 +0530

    Update server1.js

--- week11/server1.js
+++ week11/server1.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => { 
 
-    res.writeHead(200, { 'Content-Type': 'text/html' }); 
+    res.writeHead(200, {'Content-Type': 'text/html' }); 
 
     if (req.url === '/') { 
         res.end(` 
